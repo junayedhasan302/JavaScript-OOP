b44d62a17864e28df20a0f55c6b43c10205b8160
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-18 17:15:09 +0600

    Run the ts file with tsx, after --> npm i -g tsx

--- oop.js
+++ oop.js
@@ -1,7 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const student = {
     name: "Junayed",
     ID: 46,
     age: 24
 };
 console.log(student);
+//# sourceMappingURL=oop.js.map
---
+++ src/hello.ts
@@ -0,0 +1,2 @@
+const name:string = "Junayed";
+console.log(name);
